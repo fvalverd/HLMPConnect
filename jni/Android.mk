4a656fc3eb2b46ff5a5336fe578567fb796f2418
Felipe Valverde <felipe.valverde.campos@gmail.com>
2012-04-15 12:48:30 -0300

    Adding NativeTask jni for run su commands

---
+++ jni/Android.mk
@@ -0,0 +1,7 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := libnativetask
+LOCAL_SRC_FILES := android_HLMPConnect_system_NativeTask.c 
+
+include $(BUILD_SHARED_LIBRARY)
