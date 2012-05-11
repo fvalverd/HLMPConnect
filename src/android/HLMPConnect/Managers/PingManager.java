1c6dcd3ee7bcc41a8c2cc53be66a75d2a4970d9c
Felipe Valverde <felipe.valverde.campos@gmail.com>
2012-05-11 12:59:00 -0400

    Add Ping Mannager

---
+++ src/android/HLMPConnect/Managers/PingManager.java
@@ -0,0 +1,13 @@
+package android.HLMPConnect.Managers;
+
+import hlmp.CommLayer.NetUser;
+import hlmp.SubProtocol.Ping.ControlI.PingHandlerI;
+
+public class PingManager implements PingHandlerI {
+
+	public void pingResponseMessageReceived(NetUser netUser, long milliseconds) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
